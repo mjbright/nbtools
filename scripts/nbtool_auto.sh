a8e4a3f4f99f16dbbb16b4e884b8648b006be232
Michael Bright <1880109+mjbright@users.noreply.github.com>
2023-08-03 23:23:36 +0200

    Utility to rerun EXCL_FN_FILTER_NOTEBOOK automatically on notebook updates

---
+++ scripts/nbtool_auto.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+mkdir -p ~/tmp
+
+echo "---- Sourcing ~/scripts/nbtool.rc:"
+source ~/scripts/nbtool.rc
+touch ~/tmp/.nbtool.rc.read
+
+# Variables already saved from notebook:
+ls -al ~/tmp/LAB_vars.env
+source ~/tmp/LAB_vars.env
+set | grep ^LAB_.*=
+
+[ ! -f README.ipynb ] && DIE "[$PWD] No such file as README.ipynb"
+[ ! -f .converting  ] && touch .converting
+
+echo
+echo "---- Waiting for README.ipynb updates:"
+while true; do
+    [ ~/scripts/nbtool.rc -nt ~/tmp/.nbtool.rc.read ] &&
+        source ~/scripts/nbtool.rc
+
+    #[ $( find README.ipynb -newer .converting | wc -l ) != 0 ] && {
+    [ README.ipynb -nt .converting ] && {
+        echo; echo "---- EXCL_FN_LAB_ENV [$PWD]:"
+        touch .converting
+        EXCL_FN_LAB_ENV
+        ls -altr
+    }
+
+    sleep 1
+done
+
