4a982b7a2b956ef9ef74b63823f6c3efca73699c
Michael Bright <1880109+mjbright@users.noreply.github.com>
2023-08-04 13:07:50 +0200

    Added many options including cell counting by type

--- scripts/dump_notebook.sh
+++ scripts/dump_notebook.sh
@@ -1,4 +1,11 @@
 
+# TODO:
+# look at python3 option as mentioned here:
+# - https://community.checkpoint.com/t5/API-CLI-Discussion/How-to-parse-from-nested-arrays-API-JQ/td-p/81172
+#
+# jq substrings:
+# - https://stackoverflow.com/questions/61012699/jq-substr-equivalent-to-format-a-value
+
 # Show 1st source line of each cell:
 # jq -rc '.cells[].source[0]' $1
 
@@ -14,6 +21,49 @@ fi
 if [ "$1" = "-c-s1" ]; then
     shift
     jq -rc '.cells[]? | select(.cell_type=="code") | .source[0]//.source? }' $1
+    exit $?
+fi
+
+# Show keys & cell_type:
+if [ "$1" = "-k-t" ]; then
+    shift
+    jq -rc '.cells[] | { keys: keys, type: .cell_type }'  $1
+    exit $?
+fi
+
+# Count cells:
+if [ "$1" = "-#" ]; then
+    shift
+    echo "Code     Cells: " $( jq -rc 'def count(stream): reduce stream as $i (0; .+1); count(.cells[]? | select(.cell_type=="code")) ' $1)
+    echo "Markdown Cells: " $( jq -rc 'def count(stream): reduce stream as $i (0; .+1); count(.cells[]? | select(.cell_type=="markdown")) ' $1)
+    echo "TOTAL    Cells: " $( jq -rc '.cells    | length' $1 )
+    exit $?
+fi
+
+# Show code cells:
+if [ "$1" = "-code" ]; then
+    shift
+    #jq -rc '.cells[]? | to_entries | map({name:.value, index:.key}) | select(.cell_type=="code") |
+    jq -rc '.cells[]? | select(.cell_type=="code") | {
+         type: .cell_type,
+         source0: .source[]?,
+         outputs0: .outputs[0].text[0]? |.[0:80]
+    } '  $1
+    exit $?
+fi
+
+# Show metadata
+if [ "$1" = "-meta" ]; then
+    shift
+    jq -rc '.metadata' $1
+    exit $?
+fi
+
+# Show markdown cells:
+if [ "$1" = "-md" ]; then
+    shift
+    #jq -rc '.cells[]? | select(.cell_type=="markdown") | { type: .cell_type, markdown0: .source[]? }'  $1
+    jq -rc '.cells[]? | select(.cell_type=="markdown") | { md: .source[]? }'  $1
     exit $?
 fi
 
@@ -34,8 +84,30 @@ if [ "$1" = "-t" ]; then
     #jq -rc '.cells[]? | { type: .cell_type, keys: .keys() }' $1
     #jq -rc '.cells[]? | keys_unsorted }' $1
     jq -rc '.cells[] | { keys: keys, type: .cell_type }'  $1
+    # LATEST TEST:
+    #jq -rc '.cells[] | { keys: keys, type: .cell_type, source0: .source[]? }'  $1
+    jq -rc '.cells[] | { keys: keys, type: .cell_type, source0: .source[]?, outputs: .outputs[]? }'  $1
     exit $?
 fi
+
+#jq -rc '.cells[] | { type: .cell_type, source0: .source[]?, outputs: .outputs[]? }'  $1
+# Show keys & cell_type & beg. source/output:
+#if [ "$1" = "-k-t" ]; then
+    #shift
+# DEFAULT:
+    jq -rc '.cells[]? | {
+        keys:     keys,
+        type:     .cell_type,
+        source0:  .source[0]? | .[0:40],
+        #".cell_type": .outputs[0].text[0]? |.[0:40],
+        outputs0: .outputs[0].text[0]? |.[0:40]
+    }'  $1
+    exit $?
+#fi
+
+
+exit
+## --------------------------------------------------------------------------------
 
 # Seen here:
 # - https://gist.github.com/nishadhka/8b381af02662130e9adf877678d32548
