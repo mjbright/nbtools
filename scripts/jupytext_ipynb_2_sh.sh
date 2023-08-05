87566e52c4b18774f1dc7eb06ac80b9e5a513ce2
Michael Bright <1880109+mjbright@users.noreply.github.com>
2023-08-05 18:40:24 +0200

    Added jupytext pairing script

---
+++ scripts/jupytext_ipynb_2_sh.sh
@@ -0,0 +1,17 @@
+
+echo "converting README.ipynb to README.sh (percent format):"
+CMD="jupytext --to sh:percent README.ipynb"
+echo "-- $CMD"; $CMD
+ls -altr README.ipynb  README.sh
+
+## # ALL:
+## JT_OPT="--opt notebook_metadata_filter=all"
+## CMD="jupytext $JT_OPT --to sh:percent README.ipynb -o README.all.sh"
+## echo "-- $CMD"; $CMD
+
+echo "Configuring README.ipynb for pairing:"
+#CMD="jupytext --set-formats ipynb,py:percent README.ipynb"
+CMD="jupytext --set-formats ipynb,sh:percent README.ipynb"
+echo "-- $CMD"; $CMD
+
+
