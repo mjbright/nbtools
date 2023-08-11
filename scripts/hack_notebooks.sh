e84241dd3e3c7f32ceff4113c2ca6513f8a64d8b
Michael Bright <1880109+mjbright@users.noreply.github.com>
2023-08-11 19:06:17 +0200

    Dangerous script to modify notebooks - careful

---
+++ scripts/hack_notebooks.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# 1869  2023-08-11 19:03:16for FILE in ../[1-9].*/*.{sh,ipynb,md}; do echo "---- $FILE"; sed -i.bak -e 's/R_DOCKER/__R_DOCKER/g' -e 's/R_CURL/__R_CURL/g' -e 's/EXCL_FN/_/g' -e 's/TF_/__TF_/g' $FILE; done
+
+DANGEROUS
+exit
+
+cd ~/src/mjbright.tf-scenarios-private/ServeUpLabs/content/azure/
+
+for FILE in ./[1-9].*/*.{sh,ipynb,md}; do
+    echo "---- $FILE"
+    sed -i.bak \
+        -e 's/R_DOCKER/__R_DOCKER/g' \
+        -e 's/R_CURL/__R_CURL/g' \
+        -e 's/EXCL_FN/_/g' \
+        -e 's/TF_/__TF_/g' $FILE
+done
+
