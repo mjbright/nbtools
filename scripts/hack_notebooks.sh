89504e1e44857276733466c1bc8005988d4dff38
Michael Bright <1880109+mjbright@users.noreply.github.com>
2023-08-18 18:58:21 +0200

    Reverted __R_(DOCKER|CURL) to __(DOCKER|CURL)

--- scripts/hack_notebooks.sh
+++ scripts/hack_notebooks.sh
@@ -10,9 +10,14 @@ cd ~/src/mjbright.tf-scenarios-private/ServeUpLabs/content/azure/
 for FILE in ./[1-9].*/*.{sh,ipynb,md}; do
     echo "---- $FILE"
     sed -i.bak \
-        -e 's/R_DOCKER/__R_DOCKER/g' \
-        -e 's/R_CURL/__R_CURL/g' \
-        -e 's/EXCL_FN/_/g' \
-        -e 's/TF_/__TF_/g' $FILE
+        -e 's/__R_DOCKER/__DOCKER/g' \
+        -e 's/__R_CURL/__CURL/g' \
+        $FILE
+    #sed -i.bak \
+    #    -e 's/R_DOCKER/__R_DOCKER/g' \
+    #    -e 's/R_CURL/__R_CURL/g' \
+    #    -e 's/EXCL_FN/_/g' \
+    #    -e 's/TF_/__TF_/g' \
+    #   $FILE
 done
 
