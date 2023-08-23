16565d8ba9fc0df9d75f3de720ad1a5ff7eb9368
Michael Bright <1880109+mjbright@users.noreply.github.com>
2023-08-23 17:42:55 +0200

    Modularized.  Resource vars if things go wrong (suspiciously small README.md output)

--- scripts/nbtool_auto.sh
+++ scripts/nbtool_auto.sh
@@ -2,44 +2,61 @@
 
 mkdir -p ~/tmp
 
-die() { echo "$0: die - $*" >&2; exit 1; }
-
 DIR=$( basename $PWD )
 
-echo "----[$DIR] Sourcing ~/scripts/nbtool.rc:"
-source ~/scripts/nbtool.rc
-touch ~/tmp/.nbtool.rc.read ~/tmp/.nbtool.py.read
+die() { echo "$0: die - $*" >&2; exit 1; }
 
-# Variables already saved from notebook:
-ls -al ~/tmp/LAB_vars.env
-source ~/tmp/LAB_vars.env
+CHECK_FILE_SIZE() {
+    local FILE=$1; shift;
+    local MIN_BYTES=$1; shift;
 
-echo "==========================="
-set | grep -E "^(LAB_|NB_DIR)"
-echo "==========================="
+    #SIZE=$( stat -c %s $FILE )
+    SIZE=$( wc -c < $FILE )
+    [[ $SIZE -lt $MIN_BYTES ]] && {
+        echo "File looks suspiciously small"
+        ls -al $FILE
+        return 1
+    }
+    return 0
+}
 
-DIR_NUM=${DIR%.*}
-if [ "$DIR_NUM" != "$LAB_NUM" ]; then
-    echo "DIR_NUM:'$DIR_NUM' != LAB_NUM:'$LAB_NUM'"
-    echo "Is this the correct directory       ?? [${PWD##*/}]"
-    echo "Do the above variables look correct ??"
-    echo -n "Enter y/n> "
-    read DUMMY
-    [ "$DUMMY" != "y" ] && exit
-fi
+SOURCE_VARIABLES() {
+    echo "----[$DIR] Sourcing ~/scripts/nbtool.rc:"
+    source ~/scripts/nbtool.rc
+    touch ~/tmp/.nbtool.rc.read ~/tmp/.nbtool.py.read
 
-[ ! -f README.ipynb ] && DIE "[$DIR] No such file as README.ipynb"
+    # Variables already saved from notebook:
+    ls -al ~/tmp/LAB_vars.env
+    source ~/tmp/LAB_vars.env
 
-LANG=$( jq -r '.metadata.kernelspec.language' README.ipynb )
-case $LANG in
-    bash)
-        [ ! -f README.sh ] && { echo "Setting up pairing to bash";
-            echo "Configuring README.ipynb for pairing:"
-            CMD="jupytext --set-formats ipynb,sh:percent README.ipynb"
-            echo "-- $CMD"; $CMD
-        } ;;
-esac
+    echo "==========================="
+    set | grep -E "^(LAB_|NB_DIR)"
+    echo "==========================="
 
+    DIR_NUM=${DIR%.*}
+    if [ "$DIR_NUM" != "$LAB_NUM" ]; then
+        echo "DIR_NUM:'$DIR_NUM' != LAB_NUM:'$LAB_NUM'"
+        echo "Is this the correct directory       ?? [${PWD##*/}]"
+        echo "Do the above variables look correct ??"
+        echo -n "Enter y/n> "
+        read DUMMY
+        [ "$DUMMY" != "y" ] && exit
+    fi
+
+    [ ! -f README.ipynb ] && DIE "[$DIR] No such file as README.ipynb"
+
+    LANG=$( jq -r '.metadata.kernelspec.language' README.ipynb )
+    case $LANG in
+        bash)
+            [ ! -f README.sh ] && { echo "Setting up pairing to bash";
+                echo "Configuring README.ipynb for pairing:"
+                CMD="jupytext --set-formats ipynb,sh:percent README.ipynb"
+                echo "-- $CMD"; $CMD
+            } ;;
+    esac
+}
+
+SOURCE_VARIABLES
 echo
 echo "---- Waiting for README.ipynb updates:"
 FIRST_LOOP=1
@@ -66,13 +83,18 @@ while true; do
     [ README.ipynb -nt .converting ] && { CONVERT=1; }
 
     [ $CONVERT -eq 1 ] && {
-        echo; echo "---- [$DIR] EXCL_FN_LAB_ENV:"
+        echo; echo "---- [$DIR] __FN_LAB_ENV:"
         touch .converting
-        #EXCL_FN_LAB_ENV; #EXCL_FN_FILTER_NOTEBOOK README.ipynb
-        EXCL_FN_INIT_NOTEBOOK
+        #__FN_LAB_ENV; #__FN_FILTER_NOTEBOOK README.ipynb
+        __FN_INIT_NOTEBOOK
         ls -altr
         echo "-- <$DIR_NUM/$LAB_NUM> [$DIR]"
         touch ~/tmp/.nbtool.rc.read ~/tmp/.nbtool.py.read
+    }
+
+    CHECK_FILE_SIZE README.md 1000 || {
+        echo "Resourcing variables ..."
+        SOURCE_VARIABLES
     }
 
     sleep 1
