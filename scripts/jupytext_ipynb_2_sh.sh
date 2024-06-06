e573f20fa09dbcbb03e5e5e04cc3ae3f79aa0214
Michael Bright <1880109+mjbright@users.noreply.github.com>
2024-06-06 15:20:48 +0000

    Updated to allow passing of dir/notebook as parameters

--- scripts/jupytext_ipynb_2_sh.sh
+++ scripts/jupytext_ipynb_2_sh.sh
@@ -1,22 +1,31 @@
 
+IPYNB=README.ipynb
+
 if [ -d "$1" ]; then
     cd "$1"
     pwd
+    shift
 fi
 
-echo "converting README.ipynb to README.sh (percent format):"
-CMD="jupytext --to sh:percent README.ipynb"
+case "$1" in
+    *.ipynb) IPYNB=$1; shift;;
+esac
+
+IPYNB_SH=${IPYNB%.ipynb}.sh
+
+echo "converting $IPYNB to ${IPYNB_SH} (percent format):"
+CMD="jupytext --to sh:percent $IPYNB"
 echo "-- $CMD"; $CMD
-ls -altr README.ipynb  README.sh
+ls -altr $IPYNB  ${IPYNB_SH}
 
 ## # ALL:
 ## JT_OPT="--opt notebook_metadata_filter=all"
-## CMD="jupytext $JT_OPT --to sh:percent README.ipynb -o README.all.sh"
+## CMD="jupytext $JT_OPT --to sh:percent $IPYNB -o ${IPYNB_SH}"
 ## echo "-- $CMD"; $CMD
 
-echo "Configuring README.ipynb for pairing:"
-#CMD="jupytext --set-formats ipynb,py:percent README.ipynb"
-CMD="jupytext --set-formats ipynb,sh:percent README.ipynb"
+echo "Configuring $IPYNB for pairing:"
+#CMD="jupytext --set-formats ipynb,py:percent $IPYNB"
+CMD="jupytext --set-formats ipynb,sh:percent $IPYNB"
 echo "-- $CMD"; $CMD
 
 
