1bb176faf3c8c55eddae7751c22a0e9d1ab9d22b
Michael Bright <1880109+mjbright@users.noreply.github.com>
2025-03-19 06:16:42 +0000

    test script for nbjoin.py

---
+++ scripts/nbjoin_test.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+./nbjoin.py \
+    -op ~/src/mjbright.labs-terraform-private/tf-intro/FULL.ipynb \
+    NBJOIN_TEST/IP_TF_Lab1.ipynb \
+    NBJOIN_TEST/IP_TF_Lab2.Workflow.ipynb \
+    NBJOIN_TEST/IP_TF_Lab3.ipynb \
+    NBJOIN_TEST/IP_TF_Lab4.ControlStructures.ipynb \
+    NBJOIN_TEST/IP_TF_Lab5.ipynb \
+    NBJOIN_TEST/IP_TF_Lab7.Modules.ipynb \
+    NBJOIN_TEST/IP_TF_Lab8.Import.ipynb \
+    NBJOIN_TEST/IP_TF_Lab10.Revision.ipynb \
+
