283f73b47cef23a7fef8c0ea1d191f4e5578d0be
Michael Bright <1880109+mjbright@users.noreply.github.com>
2025-05-25 09:01:51 +0000

    Added -pp/-ml: pretty-print/multi-line option

--- scripts/jq_dump_notebook.sh
+++ scripts/jq_dump_notebook.sh
@@ -9,11 +9,15 @@
 # Show 1st source line of each cell:
 # jq -rc '.cells[].source[0]' $1
 
+FORMAT="-c"
+[ "$1" = "-pp" ] && { FORMAT=""; shift; }
+[ "$1" = "-ml" ] && { FORMAT=""; shift; }
+
 # Dump all paths to all attributes:
 if [ "$1" = "-P" ]; then
     # e.g. jq_dump_notebook.sh -P ~/labs/lab9/terraform.tfstate
     shift
-    jq -c 'paths' $1
+    jq $FORMAT 'paths' $1
     exit $?
 fi
 
@@ -21,14 +25,15 @@ fi
 if [ "$1" = "-p" ]; then
     # e.g. jq_dump_notebook.sh -p kubernetes_version ~/labs/lab9/terraform.tfstate
     shift
-    jq -c 'paths | select(.[-1] == "'$1'")' $2
+    jq $FORMAT 'paths | select(.[-1] == "'$1'")' $2
     exit $?
 fi
 
 # Brutal dump of all outputs/source lines:
 if [ "$1" = "-a" ]; then
     shift
-    jq -rc '.cells[]? |
+    # -rc
+    jq $FORMAT '.cells[]? |
         { cell_type, outputs, source }' $1
     exit $?
 fi
@@ -36,31 +41,34 @@ fi
 # Show first source line of every code cell:
 if [ "$1" = "-c-s1" ]; then
     shift
-    jq -rc '.cells[]? | select(.cell_type=="code") | .source[0]//.source? }' $1
+    # -rc
+    jq $FORMAT '.cells[]? | select(.cell_type=="code") | .source[0]//.source? }' $1
     exit $?
 fi
 
 # Show keys & cell_type:
 if [ "$1" = "-k-t" ]; then
     shift
-    jq -rc '.cells[] | { keys: keys, type: .cell_type }'  $1
+    # -rc
+    jq $FORMAT '.cells[] | { keys: keys, type: .cell_type }'  $1
     exit $?
 fi
 
 # Count cells:
 if [ "$1" = "-#" ]; then
     shift
-    echo "Code     Cells: " $( jq -rc 'def count(stream): reduce stream as $i (0; .+1); count(.cells[]? | select(.cell_type=="code")) ' $1)
-    echo "Markdown Cells: " $( jq -rc 'def count(stream): reduce stream as $i (0; .+1); count(.cells[]? | select(.cell_type=="markdown")) ' $1)
-    echo "TOTAL    Cells: " $( jq -rc '.cells    | length' $1 )
+    echo "Code     Cells: " $( jq $FORMAT 'def count(stream): reduce stream as $i (0; .+1); count(.cells[]? | select(.cell_type=="code")) ' $1)
+    echo "Markdown Cells: " $( jq $FORMAT 'def count(stream): reduce stream as $i (0; .+1); count(.cells[]? | select(.cell_type=="markdown")) ' $1)
+    echo "TOTAL    Cells: " $( jq $FORMAT '.cells    | length' $1 )
     exit $?
 fi
 
 # Show code cells:
 if [ "$1" = "-code" ]; then
     shift
-    #jq -rc '.cells[]? | to_entries | map({name:.value, index:.key}) | select(.cell_type=="code") |
-    jq -rc '.cells[]? | select(.cell_type=="code") | {
+    #jq $FORMAT '.cells[]? | to_entries | map({name:.value, index:.key}) | select(.cell_type=="code") |
+    # -rc
+    jq $FORMAT '.cells[]? | select(.cell_type=="code") | {
          type: .cell_type,
          source0: .source[]?,
          outputs0: .outputs[0].text[0]? |.[0:80]
@@ -71,47 +79,51 @@ fi
 # Show metadata
 if [ "$1" = "-meta" ]; then
     shift
-    jq -rc '.metadata' $1
+    # -rc
+    jq $FORMAT '.metadata' $1
     exit $?
 fi
 
 # Show markdown cells:
 if [ "$1" = "-md" ]; then
     shift
-    #jq -rc '.cells[]? | select(.cell_type=="markdown") | { type: .cell_type, markdown0: .source[]? }'  $1
-    jq -rc '.cells[]? | select(.cell_type=="markdown") | { md: .source[]? }'  $1
+    #jq $FORMAT '.cells[]? | select(.cell_type=="markdown") | { type: .cell_type, markdown0: .source[]? }'  $1
+    # -rc
+    jq $FORMAT '.cells[]? | select(.cell_type=="markdown") | { md: .source[]? }'  $1
     exit $?
 fi
 
 # TEST
 if [ "$1" = "-t" ]; then
     shift
+    # -rc
     set -x
-    jq -rc '.cells[]? | select(.cell_type=="code") | { cell_type, source[0]//.source? }' $1
-    jq -rc '.cells[]? | select(.cell_type=="code") | { cell_type, source[0]? }' $1
-    jq -rc '.cells[]? | select(.cell_type=="code") | { cell_type, source[]? }' $1
-    jq -rc '.cells[]? | select(.cell_type=="code") | { cell_type, source? }' $1
-    jq -rc '.cells[]? | select(.cell_type=="code") | { cell_type, source }' $1
-    jq -rc '.cells[]? | { cell_type, source }' $1
-    jq -rc '.cells[]? | { cell_type, source\[0] }' $1
-    jq -rc '.cells[]? | .source[0]//.source? }' $1
-    #jq -rc '.cells[]? | { type: .cell_type, source0: .source[0]//.source? }' $1
-    jq -rc '.cells[]? | { type: .cell_type, source0: .source[0] }' $1
-    #jq -rc '.cells[]? | { type: .cell_type, keys: .keys() }' $1
-    #jq -rc '.cells[]? | keys_unsorted }' $1
-    jq -rc '.cells[] | { keys: keys, type: .cell_type }'  $1
+    jq $FORMAT '.cells[]? | select(.cell_type=="code") | { cell_type, source[0]//.source? }' $1
+    jq $FORMAT '.cells[]? | select(.cell_type=="code") | { cell_type, source[0]? }' $1
+    jq $FORMAT '.cells[]? | select(.cell_type=="code") | { cell_type, source[]? }' $1
+    jq $FORMAT '.cells[]? | select(.cell_type=="code") | { cell_type, source? }' $1
+    jq $FORMAT '.cells[]? | select(.cell_type=="code") | { cell_type, source }' $1
+    jq $FORMAT '.cells[]? | { cell_type, source }' $1
+    jq $FORMAT '.cells[]? | { cell_type, source\[0] }' $1
+    jq $FORMAT '.cells[]? | .source[0]//.source? }' $1
+    #jq $FORMAT '.cells[]? | { type: .cell_type, source0: .source[0]//.source? }' $1
+    jq $FORMAT '.cells[]? | { type: .cell_type, source0: .source[0] }' $1
+    #jq $FORMAT '.cells[]? | { type: .cell_type, keys: .keys() }' $1
+    #jq $FORMAT '.cells[]? | keys_unsorted }' $1
+    jq $FORMAT '.cells[] | { keys: keys, type: .cell_type }'  $1
     # LATEST TEST:
-    #jq -rc '.cells[] | { keys: keys, type: .cell_type, source0: .source[]? }'  $1
-    jq -rc '.cells[] | { keys: keys, type: .cell_type, source0: .source[]?, outputs: .outputs[]? }'  $1
+    #jq $FORMAT '.cells[] | { keys: keys, type: .cell_type, source0: .source[]? }'  $1
+    jq $FORMAT '.cells[] | { keys: keys, type: .cell_type, source0: .source[]?, outputs: .outputs[]? }'  $1
     exit $?
 fi
 
-#jq -rc '.cells[] | { type: .cell_type, source0: .source[]?, outputs: .outputs[]? }'  $1
+#jq $FORMAT '.cells[] | { type: .cell_type, source0: .source[]?, outputs: .outputs[]? }'  $1
 # Show keys & cell_type & beg. source/output:
 #if [ "$1" = "-k-t" ]; then
     #shift
 # DEFAULT:
-    jq -rc '.cells[]? | {
+    # -rc
+    jq $FORMAT '.cells[]? | {
         keys:     keys,
         type:     .cell_type,
         id:     .id,
@@ -135,7 +147,8 @@ exit
             | grep $PATTERN)
 
 
-jq -rc '.cells[] |
+    # -rc
+jq $FORMAT '.cells[] |
     { cell_type, outputs, source }' $1
     #{ cell_type, outputs, source }' $1
     #{ cell_type, outputs, source[] | first }' $1
